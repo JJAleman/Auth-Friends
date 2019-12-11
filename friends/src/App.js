a6b3249c50b1667f9018c684156c520e555857b4
Juan Aleman <juan.j.aleman@gmail.com>
2019-12-10 22:02:13 -0600

    created private route to friendslist

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import Login from './components/Login';
-import PrivateRoute from './components/PrivateRoute';
+import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import FriendsList from "./components/FriendsList";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Auth Friends App</h1>
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Auth Friends App</h1>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Friends</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path="/protected" component={FriendsList} />
+          <Route path="/login" component={Login} />
+          <Route component={Login}/>
+        </Switch>
+        {/* <Login /> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const FriendsList = () => {
+    return (
+        <div>
+            <h1>Made it to the friend's list!</h1>
+        </div>
+    );
+};
+
+
+export default FriendsList;
