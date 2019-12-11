e83573ca2e065c279402b7fe77ac34b5b3f0b419
Juan Aleman <juan.j.aleman@gmail.com>
2019-12-10 22:18:35 -0600

    used a GET request to get the data from the api and display in on my app

--- friends/src/axiosWithAuth.js
+++ friends/src/axiosWithAuth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default axiosWithAuth = () => {
+export const axiosWithAuth = () => {
     const token = localStorage.getItem("token");
 
     return axios.create({
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { axiosWithAuth } from "../axiosWithAuth";
 
 
 const FriendsList = () => {
+
+    const [ friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        axiosWithAuth()
+        .get("/friends")
+        .then(res => {
+            console.log("Hi List of friends", res);
+            const friendsList = res.data
+            setFriends(friendsList);
+        })
+        .catch (err => {
+          console.log("Error", err);
+        })
+      }, []);
+
+
     return (
         <div>
             <h1>Made it to the friend's list!</h1>
